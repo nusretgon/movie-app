a413ff3059648c4ecae7ca71a7a7d813dd6b8ff3
Nusret <nusretgon@gmail.com>
2024-06-17 21:51:52 +0300

    Partials directory created and designed lookig that.

--- movieapp/movies/urls.py
+++ movieapp/movies/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
+    path("",views.index),
     path("index.html", views.index),
     path("movies.html", views.movies),
     path("movies/<slug:slug>", views.movie_details)    
--- movieapp/movies/views.py
+++ movieapp/movies/views.py
@@ -1,9 +1,48 @@
 from django.shortcuts import render
-
+from datetime import date
 # Create your views here.
-
+data = {
+    "movies": [
+        {
+            "title": "film-1",
+            "description": "film description",
+            "imageUrl":"m1.jpg",
+            "slug":"film-adi-1",
+            "language": "english",
+            "date": date(2021,10,10)
+        },
+        {
+            "title": "film-2",
+            "description": "film description",
+            "imageUrl":"m1.jpg",
+            "slug":"film-adi-2",
+            "language": "english",
+            "date": date(2021,1,2)
+        },
+        {
+            "title": "film-3",
+            "description": "film description",
+            "imageUrl":"m1.jpg",
+            "slug":"film-adi-3",
+            "language": "english",
+            "date": date(2023,5,3)
+        },
+        {
+            "title": "film-4",
+            "description": "film description",
+            "imageUrl":"m1.jpg",
+            "slug":"film-adi-4",
+            "language": "english",
+            "date": date(2022,5,3)
+        }
+    ],
+    "sliders":[]
+}
 def index(request):
-    return render(request, 'index.html')
+    movies=data["movies"]
+    return render(request, 'index.html',{
+        "movies":movies
+    })
 
 def movies(request):
     return render(request, 'movies.html')
